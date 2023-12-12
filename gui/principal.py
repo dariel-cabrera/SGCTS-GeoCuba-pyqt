9b427faacafccea9ef50b9ae2915e65aeabbf18e
Iglu <125487939+dariel-cabrera@users.noreply.github.com>
2023-12-12 18:49:24 -0500

    Nuevos Cambios

--- gui/principal.py
+++ gui/principal.py
@@ -1,6 +1,6 @@
 import sys
 #Donde estan todas las fotos
-from .nimg import *
+from .nimgC import *
 from PyQt6.QtWidgets import QApplication,QMainWindow,QHeaderView
 from PyQt6.QtCore import QPropertyAnimation,QEasingCurve 
 from PyQt6 import QtCore,QtWidgets
