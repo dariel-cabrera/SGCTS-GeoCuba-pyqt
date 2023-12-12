512f64f3a3ba92ea9bda11ad1f374f4541a9d1e3
Iglu <125487939+dariel-cabrera@users.noreply.github.com>
2023-12-12 18:09:07 -0500

    Agregando principal.py y las principales librerias

---
+++ gui/principal.py
@@ -0,0 +1,10 @@
+import sys
+#Donde estan todas las fotos
+from nimg import *
+from PyQt6.QtWidgets import QApplication,QMainWindow,QHeaderView
+from PyQt6.QtCore import QPropertyAnimation,QEasingCurve 
+from PyQt6 import QtCore,QtWidgets
+from PyQt6.uic import loadUi
+
+class Principal():
+    pass
