bb51e735757ddb1d1aa0f04c84bba63a7c7de573
titocode <darielcabreralopez@gmail.com>
2025-01-13 09:39:38 -0500

    Estructura de Directorios en React Cusro de React

--- React-Curso/03-Primeros-Pasos-React/src/bases/08Imp-Expor-funcComunesDeArr.js
+++
@@ -1,7 +0,0 @@
-import { heroes } from "../data/heroes";
-
-const getHeroeById= (id)=>heroes.find((heroe)=>heroe.id===id) ;
-
-console.log(getHeroeById(2));
-
-const getHeroeByOwner= (ower)=> heroes.find({heroe} => heroe.id===owner);
