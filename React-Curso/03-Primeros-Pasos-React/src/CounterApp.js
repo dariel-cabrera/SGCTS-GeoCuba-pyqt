d511f2189844c5fc6b2435c3236757c05eaf5559
titocode <darielcabreralopez@gmail.com>
2025-01-13 17:53:13 -0500

    CounterApp Curso de React

---
+++ React-Curso/03-Primeros-Pasos-React/src/CounterApp.js
@@ -0,0 +1,24 @@
+import React from "react"; 
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) =>{
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{ value }</h2>
+
+           { console.log(value) }
+        
+        </>
+
+    )
+
+
+}
+
+CounterApp.propTypes= {
+	value: PropTypes.number.isRequired  
+    
+}
+export default CounterApp;
