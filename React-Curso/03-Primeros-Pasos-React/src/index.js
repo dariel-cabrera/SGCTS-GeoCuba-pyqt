a30b6bcc13b15f72db434cdbde17ab3dbbb4815c
titocode <darielcabreralopez@gmail.com>
2025-01-13 10:01:36 -0500

    Nuestro Primer Componente Curso de React

--- React-Curso/03-Primeros-Pasos-React/src/index.js
+++ React-Curso/03-Primeros-Pasos-React/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import PrimeraApp from './PrimeraApp';
+import './index.css';
 
+// nota: Para importar el componente PrimeraApp  se escribe la palabra se borra la 
+// ultima letra si le sale una referencia se presiona tab  y se importa sola
 
-
-const saludo= <h1>Hola Mundo </h1>
-const divRoot=document.querySelector('#app');
-
-ReactDOM.render(saludo, divRoot);
-
-console.log(saludo);
+const divRoot = document.querySelector ('#app');
+ReactDOM.render( <PrimeraApp  />, divRoot);
