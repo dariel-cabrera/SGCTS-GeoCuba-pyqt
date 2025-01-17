31a7faa47710e34195eb959cb6d56c6e63783c9f
titocode <darielcabreralopez@gmail.com>
2025-01-17 17:21:58 -0500

    Pruebas en el archivo 07-desec-arr de React

--- React-Curso/03-Primeros-Pasos-React/src/bases/07DesestructuracionDeArreglod.js
+++ React-Curso/03-Primeros-Pasos-React/src/bases/07DesestructuracionDeArreglod.js
@@ -3,20 +3,20 @@
 const personajes= ['Popi','Lala','Luis'];
 
 const[p1]=personajes;
-console.log(p1) // imprime Popi
+// console.log(p1) // imprime Popi
 
 //Si quiero extraer a Lala y no a Popi separamos por comas
 const[,p2]=personajes;
-console.log(p2) // imprime Lala
+// console.log(p2) // imprime Lala
 
 // Igual con el tercero las comas omiten la posicion del arreglo.
 
 //Funcion que retorna un Arreglo 
-const retornaArreglo = () =>{
+export const retornaArreglo = () =>{
     return ['ABC', 123];
 };
 
-const [letras,numeros]= retornaArreglo();
+// const [letras,numeros]= retornaArreglo();
 
 //Tarea 
 const useState = (valor)=>{
