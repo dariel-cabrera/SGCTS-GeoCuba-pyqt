8cc32aa0c95e24a85ef1e37a2dd2af4b9fb97cdf
titocode <darielcabreralopez@gmail.com>
2025-01-20 17:26:18 -0500

    09-Pruebas async-await Curso de React

--- React-Curso/03-Primeros-Pasos-React/src/bases/12Async-Await.js
+++ React-Curso/03-Primeros-Pasos-React/src/bases/12Async-Await.js
@@ -13,30 +13,24 @@ getImagen().then(console.log) */
 // El await espera que termine la linea de coidgo antes de que pase a la sgte 
 // linea de codigo y es obligatorio que este dentro de una func await. El codigo es sincrono 
 
-const getImagen= async () => {    // Esto es una promesa 
-    const apiKey= 'ESTO ES UN EJEMPLO';
-    const resp = await fetch(`http//akknvkdfnig.com ${apiKey}`) // Espera que se ejecute la promesa
-    const {data}= await resp.json();
-    
-    const {url}=data.images.original;
-    const img = document.createElement('img');
-    img.src=url;
-    document.body.append(img);
-
-    /*
-       Para el  Manejo de Errores con: 
+export const getImagen= async () => {    // Esto es una promesa 
+       // Para el  Manejo de Errores con: 
        try {
-       
+           const apiKey= 'ESTO ES UN EJEMPLO';
+            const resp = await fetch(`http//akknvkdfnig.com ${apiKey}`) // Espera que se ejecute la promesa
+            const {data}= await resp.json();
+            const {url}=data.images.original;
+            return url 
 
        }catch(error){
-       
+           return 'No existe'
        
        }
 
-    */
+    
     
 }
 
-getImagen();
+
 
 
