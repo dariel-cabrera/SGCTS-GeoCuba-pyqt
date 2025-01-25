de6fbbd44f4a765c88274f45a3c217ce33d46289
titocode <darielcabreralopez@gmail.com>
2025-01-25 16:57:01 -0500

    Creacion createCalculoDto en NestJS

--- src/backend/src/calculo/calculo.controller.ts
+++ src/backend/src/calculo/calculo.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { CalculoService } from './calculo.service';
+import { CreateCalculoDto } from './dto/createCalculoDto';
 
 @Controller('calculo')
-export class CalculoController {}
+export class CalculoController {
+      calculoService: CalculoService;
+      constructor(calculoService: CalculoService){
+        this.calculoService= calculoService;
+      }
+
+      @Get('/calculos')
+      getAllCalculos(){
+        return this.calculoService.getCalculo();
+      }
+
+      @Post('/calculos')
+      createCalculos(@Body() calculo:CreateCalculoDto){
+        return this.calculoService.createCalculo(calculo);
+      }
+
+      @Put('/calculos')
+      updateCalculos(){
+        return this.calculoService.updateCalculo();
+      }
+
+      @Delete('/Calculos')
+      deleteCalculos(){
+        return this.calculoService.deleteCalculo();
+      }
+
+}
--- src/backend/src/calculo/calculo.service.ts
+++ src/backend/src/calculo/calculo.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CalculoService {}
+export class CalculoService {
+    getCalculo(){
+        return "Retornando Calculos"
+    }
+
+    createCalculo(calculo){
+        return "Creando Calculos"
+    }
+
+    updateCalculo(){
+        return 'Actualizando Calculos'
+   } 
+    deleteCalculo(){
+        return 'Eliminado Calculos'    
+    }
+
+
+
+
+}
