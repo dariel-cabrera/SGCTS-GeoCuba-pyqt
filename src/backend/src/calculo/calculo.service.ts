90cb861f894a3d6de4d7a603ee71159abbe1e7a1
titocode <darielcabreralopez@gmail.com>
2025-01-26 21:51:16 -0500

    Creacion de los service en Nest Js

--- src/backend/src/calculo/calculo.service.ts
+++ src/backend/src/calculo/calculo.service.ts
@@ -1,24 +1,83 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCalculoDto } from './dto/create-Calculo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Datos } from './schemas/datos.schema';
 
 @Injectable()
-export class CalculoService {
-    getCalculo(){
-        return "Retornando Calculos"
-    }
+export class DatosService {
+  constructor(
+    @InjectModel(Datos.name) private readonly datosModel: Model<Datos>,
+  ) {}
 
-    createCalculo(calculo:CreateCalculoDto){
-        return "Creando Calculos"
-    }
+  // Crear un nuevo registro
+  async createCalculo(
+    densidad_a: number,
+    densidad_m: number,
+    indice: number,
+    coeficiente: number,
+    altura: number,
+    angulo: number,
+    aceleracion: number,
+    Q: number,
+    P: number,
+    K: number,
+  ): Promise<Datos> {
+    const nuevoDato = new this.datosModel({
+      densidad_a,
+      densidad_m,
+      indice,
+      coeficiente,
+      altura,
+      angulo,
+      aceleracion,
+      Q,
+      P,
+      K,
+    });
+    return await nuevoDato.save();
+  }
 
-    updateCalculo(){
-        return 'Actualizando Calculos'
-   } 
-    deleteCalculo(){
-        return 'Eliminado Calculos'    
-    }
+  // Actualizar un registro existente
+  async updateCalculo(
+    id: string,
+    densidad_a: number,
+    densidad_m: number,
+    indice: number,
+    coeficiente: number,
+    altura: number,
+    angulo: number,
+    aceleracion: number,
+    Q: number,
+    P: number,
+    K: number,
+  ): Promise<Datos> {
+    return await this.datosModel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          densidad_a,
+          densidad_m,
+          indice,
+          coeficiente,
+          altura,
+          angulo,
+          aceleracion,
+          Q,
+          P,
+          K,
+        },
+      },
+      { new: true }, // Retornar el documento actualizado
+    );
+  }
 
+  // Eliminar un registro
+  async deleteCalculo(id: string): Promise<any> {
+    return await this.datosModel.deleteOne({ _id: id });
+  }
 
-
-
+  // Obtener todos los registros
+  async getCalculo(): Promise<Datos[]> {
+    return await this.datosModel.find().exec();
+  }
 }
