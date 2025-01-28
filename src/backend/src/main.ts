79a283b658f225beaa85eea47dad44bec8f54918
titocode <darielcabreralopez@gmail.com>
2025-01-28 11:35:38 -0500

    Arquitectura Cliente-Servidor de la Tesis

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -5,11 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
   }))
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 
 }
 bootstrap();
