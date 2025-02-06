4ac94b2675da78a1a4b9dc12878a50d35ad2287b
titocode <darielcabreralopez@gmail.com>
2025-02-06 15:14:41 -0500

    Plantilla Nueva

--- src/backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
-  console.log(`🚀 Application is running on: http://localhost:3001`);
-}
-bootstrap();
